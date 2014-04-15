765b5aacd97b061a960d76891fbeb1b954792913
Wang Peng <15652266936@163.com>
2014-04-15 12:11:21 +0800

    2014015 12:11

--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -1,28 +1,38 @@
 #ifndef QIHOO_VECTOR_H_
 #define QIHOO_VECTOR_H_
-
+#include <stdlib.h>
+#include<iostream>
+#include <assert.h>
+#include <malloc.h>
 namespace qh
 {
+	static size_t cap_default = 1024;//vector默认容量
     template<class T>
     class vector {
     public:
         //ctor
         vector()
-            : data_(NULL), size_(0)
+            : data_(NULL), size_(0),cap_(cap_default)
         {
+			data_ = new T[cap_*sizeof(T)];
         }
 
-        explicit vector( size_t n, const T& value = T())
+        explicit vector( size_t n, const T& value = T()):cap_(cap_default)
         {
-            data_ = new T[n];
+			while(cap_<n)
+				cap_+=cap_default;
+            data_ = new T[cap_*sizeof(T)];
+			size_ = n;
             for (size_t i = 0; i < n; i++)
             {
-                data_[0] = value;
+                *(data_+i) = value;
             }
         }
 
         vector<T>& operator=(const vector<T>& rhs)
         {
+			cap_=cap_default;
+			data_ = new T[cap_*sizeof(T)];
             data_ = rhs.data_;
             size_ = rhs.size_;
         }
@@ -44,19 +54,121 @@ namespace qh
 
         // set & get
         T& operator[](size_t index);
-
+		T* begin();
+		T* end();
         // set
+		void reserve(size_t size);
         void push_back(const T& element);
         void pop_back(const T& element);
-        void resize();
-        void reserve();
+		void resize(size_t size);
+        //void resize();
+        //void reserve();
         void clear();
-        void empty();
+        bool empty();
 
     private:
         T*      data_;
-        size_t  size_;
+        size_t  size_;//容器当前大小
+		size_t  cap_;//表示容器的预置容量
     };
+
+
+	template<class T>
+	T & vector<T>::operator[](size_t index)
+	{
+		if(!(index<size_&&index>=0))
+		{
+			std::cout<<"out of range"<<std::endl;
+			assert(index<size_&&index>=0);
+		}
+		return *(data_+index);
+	}
+
+
+	template<class T>
+	T* vector<T>::begin()
+	{
+		assert(size_>0);
+		return *data_;
+	}
+
+
+	template<class T>
+	T* vector<T>::end()
+	{
+		assert(size_>0);
+		return *(data_+size_-1);
+	}
+
+
+	template<class T>
+	void vector<T>::reserve(size_t size)
+	{
+
+		assert(size>size_);
+		T *p =new T[(size-size_)*sizeof(T)];
+		if(data_==NULL)
+		{
+			data_=p;
+		}
+		else
+		{
+			memcpy(data_,p,(size-size_)*sizeof(T));
+		}
+		delete[] p;
+	}
+
+
+	template<class T>
+	void vector<T>::push_back(const T& element)
+	{
+		while(size_>cap_)//如果size大于容器的预置容量了，则将预置容量增加一倍
+		{
+			cap_+=cap_default;
+			reserve(cap_);
+		}
+		*(data_+size_+1) = element;
+		size_++;
+	}
+
+
+	template<class T>
+	void vector<T>::pop_back(const T& element)
+	{
+		assert(size_);
+		size_--;
+	}
+
+
+	template<class T>
+	void vector<T>::resize(size_t size)
+	{
+		if(size>size_)
+		{
+			for(size_t i=size_;i<size;i++)
+			{
+				data_[i]=0;
+			}
+			size_=size;
+		}
+		else
+		{
+			size_=size;
+		}
+	}
+
+
+	template<class T>
+	void vector<T>::clear()
+	{
+		size_=0;
+	}
+
+	template<class T>
+	bool vector<T>::empty()
+	{
+		return size_>0?true:false;
+	}
 }
 
 #endif
